59752c8a9c3dbb858deaef0d28425127d565c394
Dan Kreft <dan@kreft.net>
2018-07-23 16:18:17 -0700

    Removed superfluous workflow reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux'
 
 import ClockReducer from './clock-reducer'
 import TaskReducer from './task-reducer'
-import WorkflowReducer from './workflow-reducer'
 
 export default combineReducers({
   clock: ClockReducer,
   tasks: TaskReducer,
-  workflow: WorkflowReducer,
 })
